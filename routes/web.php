7adff479a33b46e5a7b701f5a1c465009ae2a031
Your Name <you@example.com>
2022-04-27 16:16:43 -0500

    Evidencia paises

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,58 @@ Route::get('/', function () {
 
 //Primera ruta
 //Clase Route->metodo get
-Route::get('hola', function () {
-    echo '<h1>Hola</h1>';
+Route::get('paises', function () {
+    $paises = [
+        "Colombia" => [ 
+            "cap" => "Bogota", 
+            "mon" => "Peso Colombiano",
+            "pob" => "50'880.000",
+            "ciudades" => [
+                "Ibagué",
+                "Medellin",
+                "Cali"
+            ]
+        ],
+        "Peru" => [
+            "cap" => "Lima", 
+            "mon" => "Soles",
+            "pob" => "32'970.000",
+            "ciudades" => [
+                "Ayacucho",
+                "Cusco",
+                "Arequipa"
+            ]
+        ], 
+        "Paraguay" => [
+            "cap" => "Asuncion", 
+            "mon" => "Guaraní paraguayo",
+            "pob" => "7'133.000",
+            "ciudades" => [
+                "Ciudad del Este",
+                "Encarnacion",
+                "Areguá"
+            ]
+        ],
+        "Ecuador" => [ 
+            "cap" => "Quito", 
+            "mon" => "Dolar",
+            "pob" => "17'400.000",
+            "ciudades" => [
+                "Guayaquil",
+                "Ibarra"
+            ]
+        ],
+        "Brasil" => [
+            "cap" => "Brasilia", 
+            "mon" => "Reales",
+            "pob" => "212'600.000",
+            "ciudades" => [
+                "Sao Paulo",
+                "Rio de Janeiro",
+                "Belo Horizonte"
+            ]   
+        ]   
+    ];
+
+    return view('Paises') ->with('paises', $paises);
 }) ;
